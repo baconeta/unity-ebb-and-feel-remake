802feac0cea6b8f34eaf7e492e83fd000301ba5f
Josh Pearson <36744690+baconeta@users.noreply.github.com>
2022-02-10 21:07:22 +0200

    smooth lerp camera with screen bounds

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -5,6 +5,8 @@ public class FollowPlayer : MonoBehaviour
     public Transform targetPlayer;
     public Vector3 offset;
     [Range(1, 10)] public float smoothness;
+    public Vector3 lowerBounds;
+    public Vector3 upperBounds;
 
     // Update is called once per frame
     private void FixedUpdate()
@@ -15,7 +17,14 @@ public class FollowPlayer : MonoBehaviour
     private void Follow()
     {
         Vector3 targetPosition = targetPlayer.position + offset;
-        Vector3 smoothPosition = Vector3.Lerp(transform.position, targetPosition, smoothness * Time.fixedDeltaTime);
+
+        Vector3 boundPosition = new Vector3(
+            Mathf.Clamp(targetPosition.x, lowerBounds.x, upperBounds.x),
+            Mathf.Clamp(targetPosition.y, lowerBounds.y, upperBounds.y),
+            Mathf.Clamp(targetPosition.z, lowerBounds.z, upperBounds.z)
+        );
+
+        Vector3 smoothPosition = Vector3.Lerp(transform.position, boundPosition, smoothness * Time.fixedDeltaTime);
         transform.position = smoothPosition;
     }
 }
