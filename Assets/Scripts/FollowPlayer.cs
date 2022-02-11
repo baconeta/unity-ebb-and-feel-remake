bdf1fe935c093cd18fcd4b56a31ee44acb8cf281
Josh Pearson <36744690+baconeta@users.noreply.github.com>
2022-02-11 19:15:32 +0200

    fix camera tracking boundaries

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 public class FollowPlayer : MonoBehaviour
@@ -5,8 +6,25 @@ public class FollowPlayer : MonoBehaviour
     public Transform targetPlayer;
     public Vector3 offset;
     [Range(1, 10)] public float smoothness;
-    public Vector3 lowerBounds;
-    public Vector3 upperBounds;
+    public Vector3 lowerLevelBounds;
+    public Vector3 upperLevelBounds;
+    private float _halfViewport;
+    private Vector3 _lowerCameraBound;
+    private Vector3 _upperCameraBound;
+
+    private void Start()
+    {
+        Camera cameraComponent = gameObject.GetComponent<Camera>();
+        float orthographicSize = cameraComponent.orthographicSize;
+        _halfViewport = orthographicSize * cameraComponent.aspect;
+        
+        _lowerCameraBound.x = lowerLevelBounds.x + _halfViewport;
+        _lowerCameraBound.y = lowerLevelBounds.y + orthographicSize;
+        _upperCameraBound.x = upperLevelBounds.x - _halfViewport;
+        _upperCameraBound.y = upperLevelBounds.y - orthographicSize;
+        _lowerCameraBound.z = lowerLevelBounds.z;
+        _upperCameraBound.z = upperLevelBounds.z;
+    }
 
     // Update is called once per frame
     private void FixedUpdate()
@@ -19,9 +37,9 @@ public class FollowPlayer : MonoBehaviour
         Vector3 targetPosition = targetPlayer.position + offset;
 
         Vector3 boundPosition = new Vector3(
-            Mathf.Clamp(targetPosition.x, lowerBounds.x, upperBounds.x),
-            Mathf.Clamp(targetPosition.y, lowerBounds.y, upperBounds.y),
-            Mathf.Clamp(targetPosition.z, lowerBounds.z, upperBounds.z)
+            Mathf.Clamp(targetPosition.x, _lowerCameraBound.x, _upperCameraBound.x),
+            Mathf.Clamp(targetPosition.y, _lowerCameraBound.y, _upperCameraBound.y),
+            Mathf.Clamp(targetPosition.z, _lowerCameraBound.z, _upperCameraBound.z)
         );
 
         Vector3 smoothPosition = Vector3.Lerp(transform.position, boundPosition, smoothness * Time.fixedDeltaTime);
