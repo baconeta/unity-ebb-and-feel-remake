487521f113fd8303e8c5a265662bd260fa2ccb3f
Josh Pearson <36744690+baconeta@users.noreply.github.com>
2022-03-08 21:27:17 +0200

    add start of sanity manager

---
+++ Assets/Scripts/SanityManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SanityManager : MonoBehaviour
+{
+    public int maxSanity = 100;
+    public int minSanity = 0;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        // global sanity manager
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
