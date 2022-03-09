26358ca0e78591545ba16ffb0bfb7bb105117300
Josh Pearson <36744690+baconeta@users.noreply.github.com>
2022-03-09 17:32:42 +0200

    add flowers

---
+++ Assets/Scripts/FlowerController.cs
@@ -0,0 +1,58 @@
+using System;
+using UnityEngine;
+
+public class FlowerController : MonoBehaviour
+{
+    private SanityManager _gameSanityManager;
+
+    public bool insaneFlowers;
+    public float cooldownInSeconds;
+    private float _timeSinceLastSanityTick;
+    public float sanityEffectPerCooldown;
+    private bool _playerTouching;
+    private float _sanityDrainPerSecond;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        _gameSanityManager = FindObjectOfType<SanityManager>();
+        _playerTouching = false;
+        _timeSinceLastSanityTick = 0.0f;
+        _sanityDrainPerSecond = _gameSanityManager.sanityDrainPerSecond;
+    }
+
+    private void Update()
+    {
+        _timeSinceLastSanityTick += Time.deltaTime;
+
+        if (!_playerTouching) return;
+        if (_timeSinceLastSanityTick < cooldownInSeconds) return;
+
+        _timeSinceLastSanityTick = 0.0f;
+
+        if (insaneFlowers)
+        {
+            _gameSanityManager.AddSanity(-sanityEffectPerCooldown + _sanityDrainPerSecond);
+        }
+        else
+        {
+            _gameSanityManager.AddSanity(sanityEffectPerCooldown + _sanityDrainPerSecond);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.CompareTag("Player"))
+        {
+            _playerTouching = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D col)
+    {
+        if (col.gameObject.CompareTag("Player"))
+        {
+            _playerTouching = false;
+        }
+    }
+}
