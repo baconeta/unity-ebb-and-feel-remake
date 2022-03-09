76cb47d8962598e0f336e9362c5ecb564a32ea16
Josh Pearson <36744690+baconeta@users.noreply.github.com>
2022-03-09 13:40:07 +0200

    Update SanityManager.cs
    add sanity manager basics

--- Assets/Scripts/SanityManager.cs
+++ Assets/Scripts/SanityManager.cs
@@ -4,19 +4,60 @@ using UnityEngine;
 
 public class SanityManager : MonoBehaviour
 {
-    public int maxSanity = 100;
-    public int minSanity = 0;
-    
+    public float maxSanity = 100.0f;
+    public float minSanity = 0;
+    public float startingSanity = 75.0f;
+    private float _currentSanity;
+    public float sanityDrainPerSecond = 1.0f;
+    public float minDrainSanity = 25.0f;
+    private bool _sanityDraining;
+
+    private float _currentTimer;
+
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
         // global sanity manager
-       
+        _currentSanity = startingSanity;
+        _currentTimer = 0.0f;
+        _sanityDraining = _currentSanity > minDrainSanity;
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        // The sanity manager per tick needs to manage what the current sanity
+        _currentTimer += Time.deltaTime;
+
+        if (_currentTimer < 1.0f) return;
+
+        if (_sanityDraining)
+        {
+            _currentSanity -= sanityDrainPerSecond;
+            if (_currentSanity > minDrainSanity)
+            {
+                _currentSanity = minDrainSanity;
+                _sanityDraining = false;
+            }
+        }
+    }
+
+    public void AddSanity(float sanityToAdd)
+    {
+        _currentSanity += sanityToAdd;
+        if (_currentSanity > maxSanity)
+        {
+            _currentSanity = maxSanity;
+        }
+
+        if (_currentSanity < minSanity)
+        {
+            _currentSanity = minSanity;
+        }
+    }
+
+    public float GetSanity()
+    {
+        return _currentSanity;
     }
 }
